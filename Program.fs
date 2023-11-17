29b200f76a7432f001ca17acba1cd6492436eb60
Jangbu Sherpa <jangbusherpa@Jangbus-MacBook-Air.local>
2023-11-17 09:05:08 -0500

    first commit

---
+++ Program.fs
@@ -0,0 +1,12 @@
+﻿let calculate n =
+    let rec loop sum multiple1 =
+        if multiple1 > n then
+            sum
+        else
+            loop (sum + multiple1) (multiple1 + 3)
+
+    loop 0 3
+
+
+let result = calculate 45
+printfn "Result: %d" result
